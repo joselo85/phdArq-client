dcd2338016aaac7ed8e04cf78b254a5888c21ff0
joselo85 <joseignaciolorenzo85@gmail.com>
2022-01-05 16:43:35 -0300

    Changed Styles

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { Container, Text, VStack, Flex, HStack } from '@chakra-ui/react'
+import { Container, Text, VStack, Flex, Heading } from '@chakra-ui/react'
 import { getProjectById } from '../services/projectsService'
 import Slider from '../components/slider/Slider'
 
@@ -31,40 +31,41 @@ export default function ProjectDetails() {
 
   return (
     <>
-      <HStack justifyContent='flex-end'></HStack>
-
-      <Container maxW='container.lg' mt='5'>
-        <VStack
-          paddingTop='40px'
-          spacing='2'
-          alignItems='flex-start'
-          w='full'
-          justifyContent='space-around'
-        >
-          <Slider
+      <Flex bg='gray.100'>
+        <Container maxW='container.lg' mt='5'>
+          <VStack
+            paddingTop='40px'
+            paddingBottom='40px'
+            spacing='2'
+            alignItems='flex-start'
             w='full'
-            h='full'
-            imgs={projectData.imgs}
-            objectFit='cover'
-            objectPosition='center -100px'
-          />
-          <Flex w='full'>
-            <Text
-              textStyle='title'
-              maxW='100%'
-              flexGrow='1'
-              borderBottom='1px'
-              borderColor='#E6E6E6'
-              pt='15px'
-            >
-              {projectData.title}
+            justifyContent='space-around'
+          >
+            <Slider
+              w='full'
+              h='full'
+              imgs={projectData.imgs}
+              objectFit='cover'
+              objectPosition='center -100px'
+            />
+            <Flex w='full'>
+              <Heading
+                size='lg'
+                maxW='100%'
+                flexGrow='1'
+                borderBottom='1px'
+                borderColor='#E6E6E6'
+                pt='15px'
+              >
+                {projectData.title}
+              </Heading>
+            </Flex>
+            <Text as='p' fontSize='lg' paddingBottom='20px' paddingTop='10px'>
+              {projectData.text}
             </Text>
-          </Flex>
-          <Text as='p' fontSize='lg'>
-            {projectData.text}
-          </Text>
-        </VStack>
-      </Container>
+          </VStack>
+        </Container>
+      </Flex>
     </>
   )
 }
